c938fe0779c308e6846daa42f7b9945a314fd817
ducmanht6-hub <ducmanht6@gmail.com>
2026-05-19 20:58:40 +0700

    Update bt1.py

--- bt1.py
+++ bt1.py
@@ -1,3 +1,4 @@
+# code đề bài
 print (" --- PHẦN MỀM TÍNH TỔNG QUỸ LƯƠNG --- ")
 
 # Vòng Lặp chạy 3 lần dể nhập Lương cho 3 nhân viên
@@ -20,3 +21,18 @@ print ("= KẾT QUẢ: TỔNG NGÂN SÁCH CẦN CHUẤN BỊ LÀ:", total_budget
 total_budget = 0 đang nằm bên trong vòng lặp, nên mỗi lần lặp nó lại reset về 0.
 Phần nhập lương và cộng dồn cũng cần nằm trong for.
 Cách giải quyết: Dòng in kết quả cuối cùng nên đặt sau khi vòng lặp kết thúc. """
+
+# code sửa
+
+# Khởi tạo chiếc hộp dựng tổng tiên
+total_budget = 0
+
+# Vòng Lặp chạy 3 lần dể nhập Lương cho 3 nhân viên
+for employee_number in range(1, 4):
+
+    print ("Đang xử lý nhân viên số", employee_number)
+    # Nhập mức Lương
+    salary = int(input(" Nhập mức Lương (VNĐ): "))
+
+    # Thực hiện thao tác cộng dồn tiền vào chiếc hộp
+    total_budget = total_budget + salary
